4e002681536df395bd774bf315b2b844cd504176
huaac <huaac1@uci.edu>
2023-01-29 22:30:17 -0800

    Initial commit

---
+++ app/src/main/java/uci/cs125/zotcalcounter/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package uci.cs125.zotcalcounter.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
